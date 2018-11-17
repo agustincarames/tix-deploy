7b1c9ce6451e410aab787210167674577e9c565b
Agustin Carames <aecarames@gmail.com>
2018-11-17 18:16:22 -0300

    Add utf8 support to table definitions

--- create-tables.sql
+++ create-tables.sql
@@ -1,5 +1,7 @@
 -- TiX tables
 
+ALTER DATABASE CHARACTER SET utf8;
+
 CREATE TABLE user (
 	id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	username varchar(255) NOT NULL UNIQUE,
@@ -10,14 +12,14 @@ CREATE TABLE user (
 	recoveryToken varchar(255),
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE provider (
 	id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name varchar(255) NOT NULL,
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE location (
 	id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -27,7 +29,7 @@ CREATE TABLE location (
 	user_id integer NOT NULL REFERENCES user(id),
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE measure (
 	id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -42,10 +44,10 @@ CREATE TABLE measure (
 	user_id integer NOT NULL REFERENCES user(id),
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE location_provider (
 	id integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	location_id integer NOT NULL REFERENCES location(id),
 	provider_id integer NOT NULL REFERENCES provider(id)
-);
+) DEFAULT CHARSET=utf8;
