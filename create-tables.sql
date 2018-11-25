f1e63cfa2d9da43d0fdc0a16486e7a6d2e43527a
Agustin Carames <aecarames@gmail.com>
2018-11-25 01:52:04 -0300

    Add bitmasks to allow in-database ip-to-as queries

--- create-tables.sql
+++ create-tables.sql
@@ -51,3 +51,15 @@ CREATE TABLE location_provider (
 	location_id integer NOT NULL REFERENCES location(id),
 	provider_id integer NOT NULL REFERENCES provider(id)
 ) DEFAULT CHARSET=utf8;
+
+CREATE TABLE masks (
+	prefix integer NOT NULL,
+	bits bigint
+);
+
+INSERT INTO masks (prefix) VALUES ( 0), ( 1), ( 2), ( 3), ( 4), ( 5), ( 6), ( 7), ( 8), ( 9),
+                                  (10), (11), (12), (13), (14), (15), (16), (17), (18), (19),
+                                  (20), (21), (22), (23), (24), (25), (26), (27), (28), (29),
+                                  (30), (31), (32);
+
+UPDATE masks SET bits = pow(2, 32) - pow(2, 32 - prefix);
